5a25c767ff960b6c1cb8c901759d88ad448e184d
ericzhu <eric.zhuxiaoyu@gmail.com>
2025-08-23 10:01:54 -0400

    first contract

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -3,6 +3,16 @@ pragma solidity >0.7.0 <0.9.0;
 
 contract SimpleStorage {
 
+    uint256 private storageNumber;
+
+    function store(uint256 _storageNumber) public {
+        storageNumber = _storageNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return storageNumber;
+    }
+
 }
 
 contract SimpleStorage1 {
--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -1,14 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >0.7.0 <0.9.0;
 
-import {SimpleStorage, SimpleStorage1} from "contracts/SimpleStorage.sol";
+import {SimpleStorage} from "contracts/SimpleStorage.sol";
 
 contract StorageFactory {
-    SimpleStorage public simpleStorage;
-    SimpleStorage1 public simpleStorage1;
+    SimpleStorage[] public simpleStorageContractList;
 
     function createSimpleStorage() public {
-        simpleStorage = new SimpleStorage();
-        simpleStorage1 = new SimpleStorage1();
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        simpleStorageContractList.push(newSimpleStorageContract);
+    }
+
+    function sfStore(uint256 _contractIndex, uint256 _simpleStorageNumber) public {
+        simpleStorageContractList[_contractIndex].store(_simpleStorageNumber);
+    }
+
+    function sfRetrieveStorageNumber(uint256 _contractIndex) public view returns (uint256) {
+        return simpleStorageContractList[_contractIndex].retrieve();
     }
 }
